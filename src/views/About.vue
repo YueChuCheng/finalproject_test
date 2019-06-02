7a378eb328aed7e671944896e0ae57224d6c2aab
Yue Chu Cheng <maggie9907@gmail.com>
2019-06-03 01:10:18 +0800

    Change value finish

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <form name="form_name" id="form_name">
       <label >Name</label>
-      <input type="text" v-model="name" />
+      <input type="text" name="name" />
       <button @click="set()">Add</button>
+      </form>
   </div>
+  
 </template>
 <script >
 import {namesRef} from '../firebase';
@@ -13,10 +16,11 @@ export default {
   methods:{
 
     set(){
+      var form = document.getElementById("form_name");
       var user = firebase.auth().currentUser;
       user.providerData.forEach(function (profile) {
       firebase.database().ref('users/' + profile.uid).set({
-      username: "testsadfas",
+      username: form.name.value,
     
         });
       });
