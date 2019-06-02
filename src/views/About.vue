519b44a4ba203362c0d652c50a7eeb33717738e2
Yue Chu Cheng <maggie9907@gmail.com>
2019-06-03 01:38:12 +0800

    read data

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,35 +2,49 @@
   <div class="about">
     <h1>This is an about page</h1>
     <form name="form_name" id="form_name">
-      <label >Name</label>
-      <input type="text" name="name" />
+      <label>Name</label>
+      <input type="text" name="name">
       <button @click="set()">Add</button>
-      </form>
+    </form>
+    <br>
+
+    <label>Read</label>
+    <br>
+    <button @click="read()">read</button>
   </div>
-  
 </template>
+
+
+
 <script >
-import {namesRef} from '../firebase';
-import '../firebase'
+import { namesRef } from "../firebase";
+import "../firebase";
 export default {
-  methods:{
-
-    set(){
+  methods: {
+    set() {
       var form = document.getElementById("form_name");
-      var user = firebase.auth().currentUser;
-      user.providerData.forEach(function (profile) {
-      firebase.database().ref('users/' + profile.uid).set({
-      username: form.name.value,
-    
+      var user = firebase.auth().currentUser.uid;
+      firebase
+        .database()
+        .ref("users/" + user)
+        .set({
+          username: form.name.value
         });
-      });
-  
+    },
 
+    read() {
+      var user = firebase.auth().currentUser.uid;
+
+      return firebase
+        .database()
+        .ref("/users/" + user)
+        .once("value")
+        .then(function(snapshot) {
+          var username =
+            (snapshot.val() && snapshot.val().username) || "Anonymous";
+          console.log(username);
+        });
     }
-
-
   }
-
-
-}
+};
 </script>
